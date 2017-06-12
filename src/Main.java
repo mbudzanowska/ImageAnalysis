f9164cec71c4118085aa5bc35eaba2fba1df31b6
mbudzanowska <m.budzanowska@orange.pl>
2017-06-12 09:08:52 +0200

    RANSAC working

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Image i1 = new Reader().readImageHaraff("stop.png");
-		Image i2 = new Reader().readImageHaraff("stop2.png");
+		Image i1 = new Reader().readImageHaraff("eiffel.png");
+		Image i2 = new Reader().readImageHaraff("eiffel2.png");
 
 		JFrame frame = new JFrame();
-		Visualisator v = new Visualisator(i1, i2, new NearestNeighbourAlgorithm(i1, i2).getCoupleKeyPoints());
+		//Visualisator v = new Visualisator(i1, i2, new NearestNeighbourAlgorithm(i1, i2).getCoupleKeyPoints());
+		Visualisator v = new Visualisator(i1, i2, new RANSAC(i1, i2).getCoupleKeyPoints()); 
 		Dimension dim = v.getDimension();
 		frame.setContentPane(v);
 		frame.setVisible(true);
--- src/RANSAC.java
+++ src/RANSAC.java
@@ -9,8 +9,8 @@ public class RANSAC {
 	private static Image image_2nd;
 	private static int features_number;
 	private static final Transformation transformation_type = Transformation.AFFINE;
-	private static final int ITERATIONS_NUMBER = 20;
-	private static final int SAMPLES_NUMBER = 5;
+	private static final int ITERATIONS_NUMBER = 1000;
+	private static final double MAX_ERROR = 3;
 
 	public RANSAC(Image image_1st, Image image_2nd) {
 
@@ -30,7 +30,22 @@ public class RANSAC {
 		List<NeighbourPoints> key_points = findAllPossibleCouples();
 		System.out.println("Number of possible neighbour points: " + key_points.size());
 
+		double[][] model = chooseBestModel(key_points);
+		
+		List<NeighbourPoints> consistent_points = getAllPointsWithModel(model, key_points);
+		System.out.println("Number of consistent key points: " + consistent_points.size());
 		return consistent_points;
+	}
+
+	public List<NeighbourPoints> getAllPointsWithModel(double[][] model, List<NeighbourPoints> key_points) {
+
+		List<NeighbourPoints> consistent_poinst = new ArrayList<NeighbourPoints>();
+		for (NeighbourPoints pair : key_points) {
+			Point counted_point = countPointWithModel(model, pair.getFirstPoint());
+			if (getDistance(counted_point, pair.getSecondPoint()) < MAX_ERROR)
+				consistent_poinst.add(new NeighbourPoints(pair.getFirstPoint(), counted_point));
+		}
+		return consistent_poinst;
 	}
 
 	private void setAllClosestPointsOnSecondImage(Image image_1st, Image image_2nd) {
@@ -77,7 +92,6 @@ public class RANSAC {
 
 		double[][] best_model = null;
 		double best_score = 0;
-
 		for (int i = 0; i < ITERATIONS_NUMBER; i++) {
 			double[][] model = null;
 			NeighbourPoints[] randomly_chosen_points;
@@ -95,10 +109,9 @@ public class RANSAC {
 				break;
 			}
 			}
-
 			for (NeighbourPoints neighbour_point : key_points) {
 				double error = countError(model, neighbour_point);
-				if (error < max_error) // what the fuck is max error
+				if (error < MAX_ERROR) // what the fuck is max error
 					score++;
 			}
 
@@ -112,11 +125,26 @@ public class RANSAC {
 	}
 
 	private double countError(double[][] model, NeighbourPoints key_points) {
-		// TODO Auto-generated method stub
-		return 0;
+		Point a = countPointWithModel(model, key_points.getFirstPoint());
+		return getDistance(a, key_points.getSecondPoint());
 	}
 
-	private Matrix calculateAffineModel(NeighbourPoints[] chosen_points) {
+	private Point countPointWithModel(double[][] model, Point point) {
+
+		double x_val = model[0][0] * point.getX() + model[0][1] * point.getY() + model[0][2];
+		double y_val = model[1][0] * point.getX() + model[1][1] * point.getY() + model[1][2];
+		double z_val = model[2][0] * point.getX() + model[2][1] * point.getY() + model[2][2];
+
+		x_val = x_val / z_val;
+		y_val = y_val / z_val;
+		return new Point(x_val, y_val, null);
+	}
+
+	private double getDistance(Point a, Point b) {
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+	}
+
+	private double[][] calculateAffineModel(NeighbourPoints[] chosen_points) {
 
 		double x1 = chosen_points[0].getFirstPoint().getX();
 		double x2 = chosen_points[1].getFirstPoint().getX();
@@ -139,14 +167,20 @@ public class RANSAC {
 
 		Matrix m1 = new Matrix(image_1_matrix);
 		Matrix m2 = new Matrix(image_2_matrix);
-		
-		Matrix A = m1.inverse().times(m2);
+		Matrix A;
+		try {
+		A = m1.inverse().times(m2);
+		} catch (RuntimeException ex) {
+			A = new Matrix (9,1);
+		}
 
-		double [][] result = {A.
-		return 
+		double[][] result = { { A.get(0, 0), A.get(1, 0), A.get(2, 0) }, { A.get(3, 0), A.get(4, 0), A.get(5, 0) },
+				{ 0, 0, 1 } };
+
+		return result;
 	}
 
-	private Matrix calculatePerspectiveModel(NeighbourPoints[] chosen_points) {
+	private double[][] calculatePerspectiveModel(NeighbourPoints[] chosen_points) {
 
 		double x1 = chosen_points[0].getFirstPoint().getX();
 		double x2 = chosen_points[1].getFirstPoint().getX();
@@ -177,7 +211,17 @@ public class RANSAC {
 		Matrix m1 = new Matrix(image_1_matrix);
 		Matrix m2 = new Matrix(image_2_matrix);
 
-		return m1.inverse().times(m2);
+		Matrix A;
+		try {
+		A = m1.inverse().times(m2);
+		} catch (RuntimeException ex) {
+			A = new Matrix (9,1);
+		}
+
+		double[][] result = { { A.get(0, 0), A.get(1, 0), A.get(2, 0) }, { A.get(3, 0), A.get(4, 0), A.get(5, 0) },
+				{ A.get(6, 0), A.get(7, 0), 1 } };
+
+		return result;
 	}
 
 	private NeighbourPoints[] chosePointsRandomly(List<NeighbourPoints> key_points, int number) {
